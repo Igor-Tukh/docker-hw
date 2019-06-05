dd4e8fc0ac4b57935167b45c8d2e2a93b8035789
Igor Tukh <igor-tukh@yandex.ru>
2019-06-05 11:27:27 +0300

    files refactoring

--- increment_and_move.py
+++ increment_and_move.py
@@ -1,17 +1,19 @@
 import boto3
+import sys
 
-if len(sys.argv) != 3:
-    print("Expected arguments: [first queue name] [second queue name]")
-    print("Got only " + str(len(sys.argv) - 1) + " arguments")
-    exit(-1)
-    
-sqs = boto3.resource(service_name='sqs', endpoint_url='http://localstack:4576')
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Expected arguments: [first queue name] [second queue name]")
+        print("Got only " + str(len(sys.argv) - 1) + " arguments")
+        exit(-1)
 
-from_queue = sqs.get_queue_by_name(sys.argv[1])
-to_queue = sqs.get_queue_by_name(sys.argv[2])
+    sqs = boto3.resource(service_name='sqs', endpoint_url='http://localstack:4576')
 
-while True:
-    for message in from_queue.receive_messages():
-        value = int(message.body)
-        message.delete()
-        to_queue.send_message(str(value + 1))
+    from_queue = sqs.get_queue_by_name(sys.argv[1])
+    to_queue = sqs.get_queue_by_name(sys.argv[2])
+
+    while True:
+        for message in from_queue.receive_messages():
+            value = int(message.body)
+            message.delete()
+            to_queue.send_message(str(value + 1))
--- initialize_queues.py
+++ initialize_queues.py
@@ -1,11 +1,9 @@
 import boto3
 
-sqs = boto3.resource(service_name='sqs', endpoint_url='http://localstack:4576')
+if __name__ == '__main__':
+    sqs = boto3.resource(service_name='sqs', endpoint_url='http://localstack:4576')
 
-def create_queue(name):
-    return sqs.create_queue(name)
-    
-A_queue = sqs.create_queue('A')
-B_queue = sqs.create_queue('B')
+    A_queue = sqs.create_queue('A')
+    B_queue = sqs.create_queue('B')
 
-A_queue.send_message('1')    
+    A_queue.send_message('1')
