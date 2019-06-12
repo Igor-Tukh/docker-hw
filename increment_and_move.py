8d2e7c1e18d14cfc245ceba2fe7161fe9cc397b0
Alexandr Fedorov <fedorov2602@mail.ru>
2019-06-12 23:48:32 +0300

    Restart of services on failure

--- increment_and_move.py
+++ increment_and_move.py
@@ -8,26 +8,11 @@ if __name__ == '__main__':
         print("Got only " + str(len(sys.argv) - 1) + " arguments")
         exit(-1)
 
-    sqs = None
-    while True:
-        try:
-            sqs = boto3.resource(service_name='sqs', endpoint_url='http://localstack:4576')
-            break
-        except:
-            time.sleep(1)
-            continue
-
-    from_queue = None
-    to_queue = None
-    while True:
-        try:
-            from_queue = sqs.get_queue_by_name(QueueName=sys.argv[1])
-            to_queue = sqs.get_queue_by_name(QueueName=sys.argv[2])
-            break
-        except:
-            time.sleep(1)
-            continue
-
+    sqs = boto3.resource(service_name='sqs', endpoint_url='http://localstack:4576')
+     
+    from_queue = sqs.get_queue_by_name(QueueName=sys.argv[1])
+    to_queue = sqs.get_queue_by_name(QueueName=sys.argv[2])
+ 
     while True:
         for message in from_queue.receive_messages():
             value = int(message.body)
--- initialize_queues.py
+++ initialize_queues.py
@@ -2,15 +2,8 @@ import boto3
 import time
 
 if __name__ == '__main__':
-    sqs = None
-    while True:
-        try:
-            sqs = boto3.resource(service_name='sqs', endpoint_url='http://localstack:4576')
-            break
-        except:
-            time.sleep(1)
-            continue
-
+    sqs = boto3.resource(service_name='sqs', endpoint_url='http://localstack:4576')
+    
     A_queue = sqs.create_queue(QueueName='A')
     B_queue = sqs.create_queue(QueueName='B')
 
